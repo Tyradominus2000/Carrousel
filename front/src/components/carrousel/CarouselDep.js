58734a4dc11cf1c2fc87131381066fcd54c50bc9
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-09-06 16:13:39 +0200

    fix : scroll fix

--- front/src/components/carrousel/CarouselDep.js
+++ front/src/components/carrousel/CarouselDep.js
@@ -1,79 +1,84 @@
 // Carousel.js
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Carousel.scss";
 
 const Carousel = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const scrollAmount = window.innerWidth/6; // Increase this value to scroll more
-  console.log(scrollAmount);
-  const Image = document.getElementsByClassName("imgCarrousel");
+  const carouselRef = useRef(null);
+
   useEffect(() => {
     // Calculate initial offset to center the selected image
-    setCurrentIndex(0);
+    const initialOffset = Math.floor((items.length - 1) / 2);
+    setCurrentIndex(initialOffset);
   }, [items]);
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-    // Scroll more to the right when clicking the next button
-    setScrollPosition(
-      (prevScrollPosition) => prevScrollPosition + scrollAmount
-    );
+    // Scroll to center of the selected item
+    scrollToCenter(currentIndex + 1);
   };
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? items.length - 1 : prevIndex - 1
     );
-    // Scroll more to the left when clicking the previous button
-    setScrollPosition(
-      (prevScrollPosition) => prevScrollPosition - scrollAmount
-    );
+    // Scroll to center of the selected item
+    scrollToCenter(currentIndex - 1);
   };
 
   const goToSlide = (index) => {
     setCurrentIndex(index);
-    document.getElementsByClassName("carousel-track").scrollLeft += 20;
-    console.log(Image[index]);
+    // Scroll to center of the selected item
+    scrollToCenter(index);
+  };
+
+  const scrollToCenter = (index) => {
+    if (carouselRef.current) {
+      const selectedElement = carouselRef.current.querySelector(
+        `.carousel-slide:nth-child(${index + 1})`
+      );
+      if (selectedElement) {
+        const containerWidth = carouselRef.current.offsetWidth;
+        const itemWidth = selectedElement.offsetWidth;
+        const scrollLeft =
+          selectedElement.offsetLeft - (containerWidth - itemWidth) / 2;
+
+        // Smoothly scroll to the center
+        carouselRef.current.scrollTo({
+          left: scrollLeft,
+          behavior: "smooth",
+        });
+      }
+    }
   };
 
   return (
-    <div className="carousel">
-      <div className="carousel-arrow left" onClick={prevSlide}>
-        &lt;
-      </div>
-      <div
-        className="carousel-track"
-        style={{
-          transform: `translateX(-${
-            currentIndex * (scrollAmount / items.length)
-          }%)`,
-        }}
-      >
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={`carousel-slide ${
-              index === currentIndex ? "active" : ""
-            }`}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%", // Ensure all elements take full width
-            }}
-          >
-            <img
-              className="imgCarrousel"
-              src={item}
-              alt={`Slide ${index}`}
-              style={{ maxWidth: `${currentIndex === index ? "100%" : "80%"}` }}
-            />
-          </div>
-        ))}
-      </div>
-      <div className="carousel-arrow right" onClick={nextSlide}>
-        &gt;
+    <div className="carousel-container">
+      <div className="carousel" ref={carouselRef}>
+        <div className="carousel-arrow left" onClick={prevSlide}>
+          &lt;
+        </div>
+        <div className="carousel-track">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`carousel-slide ${
+                index === currentIndex ? "active" : ""
+              }`}
+            >
+              <img
+                src={item}
+                alt={`Slide ${index}`}
+                style={{
+                  maxWidth: `${currentIndex === index ? "100%" : "80%"}`,
+                }}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="carousel-arrow right" onClick={nextSlide}>
+          &gt;
+        </div>
       </div>
       <div className="carousel-dots">
         {items.map((_, index) => (
@@ -84,7 +89,6 @@ const Carousel = ({ items }) => {
           />
         ))}
       </div>
-      <div className="scroll-position">Scroll Position: {scrollPosition}px</div>
     </div>
   );
 };
